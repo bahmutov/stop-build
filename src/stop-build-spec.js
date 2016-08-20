82e4a3f360e7ce8ff82e935fa6e851c98e9f6487
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-08-19 22:52:42 -0400

    chore(test): testing

--- src/stop-build-spec.js
+++ src/stop-build-spec.js
@@ -1,7 +1,9 @@
 'use strict'
 
 /* global describe, it */
-describe('', () => {
-  it('', () => {
+describe('bin script', () => {
+  it('stops the build', (done) => {
+    setTimeout(done, 200)
+    require('..')
   })
 })
