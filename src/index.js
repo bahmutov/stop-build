8db89bbc0c2e650d76468f959c75dd0b6b388e4e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-22 08:59:09 -0400

    chore(format): use prettier-standard before running standard

--- src/index.js
+++ src/index.js
@@ -15,22 +15,27 @@ function changedFileInfo (change) {
 
 function changesFiles (changes) {
   // see ggit.changedFiles docs
-  const all = [].concat(changes.A).concat(changes.C)
-    .concat(changes.M).concat(changes.D).filter(notEmpty)
+  const all = []
+    .concat(changes.A)
+    .concat(changes.C)
+    .concat(changes.M)
+    .concat(changes.D)
+    .filter(notEmpty)
   return all.map(changedFileInfo)
 }
 
-ggit.hasChanges()
-  .then(function (changed) {
-    if (!changed) {
-      return
-    }
-    return ggit.changedFiles()
-      .then(function (changes) {
-        const files = changesFiles(changes)
-        console.error('⚠️ there are %d changed %s',
-          files.length, pluralize('file', files.length))
-        console.error(files.join('\n'))
-        process.exit(-1)
-      })
+ggit.hasChanges().then(function (changed) {
+  if (!changed) {
+    return
+  }
+  return ggit.changedFiles().then(function (changes) {
+    const files = changesFiles(changes)
+    console.error(
+      '⚠️ there are %d changed %s',
+      files.length,
+      pluralize('file', files.length)
+    )
+    console.error(files.join('\n'))
+    process.exit(-1)
   })
+})
--- src/stop-build-spec.js
+++ src/stop-build-spec.js
@@ -2,7 +2,7 @@
 
 /* global describe, it */
 describe('bin script', () => {
-  it('stops the build', (done) => {
+  it('stops the build', done => {
     console.log('run the test only after committing the files or on CI')
     setTimeout(done, 200)
     require('..')
